6862ce7b961e85cc63d4a39d037fd54a526b2062
Stanley Winata <winatastanley355@gmail.com>
2024-12-20 14:03:02 +0700

    Find user route

--- src/users/routes.rs
+++ src/users/routes.rs
@@ -1,11 +1,12 @@
 use super::services::User;
 use crate::db::DbPool;
 use crate::users::request::CreateUserPayload;
-use axum::extract::State;
+use axum::extract::{Query, State};
 use axum::http::StatusCode;
-use axum::routing::post;
+use axum::routing::{get, post};
 use axum::{Json, Router};
 use crate::middleware::ApiResponse;
+use serde::{Deserialize};
 
 async fn create_user_route(
     // this argument tells axum to parse the request body
@@ -13,7 +14,6 @@ async fn create_user_route(
     State(pool): State<DbPool>,
     Json(payload): Json<CreateUserPayload>,
 ) -> (StatusCode, Json<ApiResponse<User>>) {
-    println!("{:?}", payload);
     let check_user = User::find_user_by_email(&pool, &payload.email);
     match check_user {
         Ok(_) => {
@@ -23,7 +23,7 @@ async fn create_user_route(
             let create_user = User::create_user(&pool, &payload.supertokens_user_id, &payload.email);
             match create_user {
                 Ok(user) => {
-                    ApiResponse::new(StatusCode::CREATED, Some(user), "User created").send()
+                    ApiResponse::new(StatusCode::OK, Some(user), "User created").send()
                 }
                 Err(e) => ApiResponse::new(StatusCode::INTERNAL_SERVER_ERROR, None, &e.to_string()).send(),
             }
@@ -31,6 +31,23 @@ async fn create_user_route(
     }
 }
 
+#[derive(Deserialize)]
+struct FindUserQuery {
+    email: String,
+}
+
+async fn find_user_route(
+    State(pool): State<DbPool>,
+    query: Query<FindUserQuery> 
+) -> (StatusCode, Json<ApiResponse<User>>) {
+    let check_user = User::find_user_by_email(&pool, &query.email);
+    
+    match check_user { 
+        Ok(user) => ApiResponse::new(StatusCode::FOUND, Some(user), "User found").send(),
+        Err(e) => ApiResponse::new(StatusCode::BAD_REQUEST, None, &e.to_string()).send(),
+    }
+}
+
 pub fn user_routes() -> Router<DbPool> {
-    Router::new().route("/create-user", post(create_user_route))
+    Router::new().route("/create-user", post(create_user_route)).route("/find-user", get(find_user_route))
 }
