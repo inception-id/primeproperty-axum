10608d062df73b4a6f2cb050515a6a9e64e5305d
Stanley Winata <winatastanley355@gmail.com>
2025-03-01 10:08:12 +0700

    Tars chat rooms

--- src/tars_chat_rooms/controller.rs
+++ src/tars_chat_rooms/controller.rs
@@ -31,6 +31,23 @@ pub(crate) struct CreateTarsChatRoomPayload {
     pub ai_model_id: i32,
 }
 
+async fn create_tars_chat_room_route(
+    State(pool): State<DbPool>,
+    headers: HeaderMap,
+    Json(payload): Json<CreateTarsChatRoomPayload>,
+) -> TarsChatRoomResponse {
+    let user_id = extract_header_user_id(headers).expect("Could not extract user id");
+
+    match TarsChatRoom::create(&pool, &user_id, &payload) {
+        Ok(room) => ApiResponse::new(StatusCode::CREATED, Some(room), "ok").send(),
+        Err(err) => {
+            ApiResponse::new(StatusCode::INTERNAL_SERVER_ERROR, None, &err.to_string()).send()
+        }
+    }
+}
+
 pub fn tars_chat_rooms_routes() -> Router<DbPool> {
-    Router::new().route("/find-all", get(find_all_tars_chat_rooms_routes))
+    Router::new()
+        .route("/find-all", get(find_all_tars_chat_rooms_routes))
+        .route("/create", post(create_tars_chat_room_route))
 }
