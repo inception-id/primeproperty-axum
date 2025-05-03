1eec224c044f20c061b1ccf11c1e55a79631bafb
Stanley Winata <winatastanley355@gmail.com>
2025-05-03 16:15:37 +0700

    Create lead with api key

--- src/leads/controller.rs
+++ src/leads/controller.rs
@@ -3,6 +3,7 @@ use crate::properties::Property;
 use crate::traits::Crud;
 use crate::{db::DbPool, middleware::AxumResponse, schema};
 use axum::extract::{Json, State};
+use axum::http::HeaderMap;
 use axum::routing::post;
 use axum::Router;
 use diesel::prelude::Insertable;
@@ -22,8 +23,22 @@ pub struct CreateLeadPayload {
 
 async fn create_lead(
     State(pool): State<DbPool>,
+    headers: HeaderMap,
     Json(payload): Json<CreateLeadPayload>,
 ) -> AxumResponse<Lead> {
+    let api_key_option = headers.get("x-api-key");
+
+    let api_key = match api_key_option {
+        Some(key) => key.to_str().unwrap_or(""),
+        None => return JsonResponse::send(401, None, None),
+    };
+
+    let leads_api_key = std::env::var("API_KEY_LEADS").expect("Missing API_KEY_LEADS");
+
+    if api_key != leads_api_key {
+        return JsonResponse::send(401, None, None);
+    }
+
     let property = match Property::find_one_by_id(&pool, &payload.property_id) {
         Ok(property) if property.0.user_id == payload.user_id => property,
         _ => return JsonResponse::send(400, None, None),
